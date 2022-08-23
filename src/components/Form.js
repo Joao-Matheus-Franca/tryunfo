ce15ce49e17c81102821d37a284e3dd739440e00
João Matheus Silva Franca <matheussfranca@live.com>
2022-08-23 16:32:22 -0300

    Concluído: Requisito 02

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,28 +1,96 @@
 import React from 'react';
+import propTypes from 'prop-types';
 import MyInputs from './MyInputs';
 
 class Form extends React.Component {
   render() {
+    const { cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3,
+      cardImage, cardRare, cardTrunfo, isSaveButtonDisabled,
+      onInputChange, onSaveButtonClick } = this.props;
+    // const { hasTrunfo } = this.props;
     return (
       <>
-        <MyInputs testId="name-input" type="text" />
-        <MyInputs testId="description-input" type="textarea" />
-        <MyInputs testId="attr1-input" type="number" />
-        <MyInputs testId="attr2-input" type="number" />
-        <MyInputs testId="attr3-input" type="number" />
-        <MyInputs testId="image-input" type="text" />
+        <MyInputs
+          testId="name-input"
+          type="text"
+          value={ cardName }
+          onChange={ onInputChange }
+        />
+        <MyInputs
+          testId="description-input"
+          type="textarea"
+          value={ cardDescription }
+          onChange={ onInputChange }
+        />
+        <MyInputs
+          testId="attr1-input"
+          type="number"
+          value={ cardAttr1 }
+          onChange={ onInputChange }
+        />
+        <MyInputs
+          testId="attr2-input"
+          type="number"
+          value={ cardAttr2 }
+          onChange={ onInputChange }
+        />
+        <MyInputs
+          testId="attr3-input"
+          type="number"
+          value={ cardAttr3 }
+          onChange={ onInputChange }
+        />
+        <MyInputs
+          testId="image-input"
+          type="text"
+          value={ cardImage }
+          onChange={ onInputChange }
+        />
         <label htmlFor="rare-input">
-          <select data-testid="rare-input">
+          <select
+            data-testid="rare-input"
+            value={ cardRare }
+            onChange={ onInputChange }
+          >
             <option>normal</option>
             <option>raro</option>
             <option>muito raro</option>
           </select>
         </label>
-        <MyInputs testId="trunfo-input" type="checkbox" />
-        <MyInputs testId="save-button" type="button" />
+        <label htmlFor="trunfo-input">
+          <input
+            data-testId="trunfo-input"
+            type="checkbox"
+            checked={ cardTrunfo }
+            onChange={ onInputChange }
+          />
+        </label>
+        <label htmlFor="save-button">
+          <input
+            data-testId="save-button"
+            type="button"
+            disabled={ isSaveButtonDisabled }
+            onClick={ onSaveButtonClick }
+          />
+        </label>
       </>
     );
   }
 }
+
+Form.propTypes = {
+  cardName: propTypes.string.isRequired,
+  cardDescription: propTypes.string.isRequired,
+  cardAttr1: propTypes.string.isRequired,
+  cardAttr2: propTypes.string.isRequired,
+  cardAttr3: propTypes.string.isRequired,
+  cardImage: propTypes.string.isRequired,
+  cardRare: propTypes.string.isRequired,
+  cardTrunfo: propTypes.bool.isRequired,
+  // hasTrunfo: propTypes.bool.isRequired,
+  isSaveButtonDisabled: propTypes.bool.isRequired,
+  onInputChange: propTypes.func.isRequired,
+  onSaveButtonClick: propTypes.func.isRequired,
+};
 
 export default Form;
--- src/components/MyInputs.js
+++ src/components/MyInputs.js
@@ -3,11 +3,16 @@ import propTypes from 'prop-types';
 
 class MyInputs extends React.Component {
   render() {
-    const { testId, type, description } = this.props;
+    const { testId, type, description, value, onChange } = this.props;
     return (
       <label htmlFor={ testId }>
         { description }
-        <input data-testId={ testId } type={ type } />
+        <input
+          data-testId={ testId }
+          type={ type }
+          value={ value }
+          onChange={ onChange }
+        />
       </label>
     );
   }
@@ -16,6 +21,8 @@ class MyInputs extends React.Component {
 MyInputs.propTypes = {
   testId: propTypes.string.isRequired,
   type: propTypes.string.isRequired,
+  value: propTypes.string.isRequired,
+  onChange: propTypes.func.isRequired,
   description: propTypes.string,
 };
 
